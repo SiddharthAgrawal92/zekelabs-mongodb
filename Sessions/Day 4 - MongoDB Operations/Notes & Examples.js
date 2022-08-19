b669ee279c85e047241eb12563c45da611e4f6bc
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-08-20 00:16:16 +0530

    Day 4 - MongoDB Operations

---
+++ Sessions/Day 4 - MongoDB Operations/Notes & Examples.js
@@ -0,0 +1,16 @@
+//Data type object 
+[
+    {
+        isActive: false,
+        discount: 1.5,
+        min: 1,
+        max: 5,
+        operators: ["Macdonalds", "Dominos"],
+        obj: {
+            key1: "value1",
+            key2: []
+        },
+        adminAccess: null,
+        regExp: /%g/
+    }
+]
